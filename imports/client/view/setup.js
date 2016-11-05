a223d4b0e103c2759552c708e79c267732cfdbb8
johnelmer <johnelmer_loretizo@yahoo.com>
2016-11-05 13:35:16 +0800

    Initial commit for angular

---
+++ imports/client/view/setup.js
@@ -0,0 +1,8 @@
+import angular from 'angular'
+import angularMeteor from 'angular-meteor'
+import bootstrap from 'angular-ui-bootstrap'
+import uiRouter from 'angular-ui-router'
+
+export default angular.module(name, [
+  angularMeteor
+]);
